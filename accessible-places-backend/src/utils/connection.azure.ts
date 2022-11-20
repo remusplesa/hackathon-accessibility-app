3edb365477728746b9192cce650ad84a1e0107c3
Andrei Remus Plesa <80645758+remusplesa@users.noreply.github.com>
2022-11-20 12:16:10 +0200

    fix: dotenv config

--- accessible-places-backend/src/utils/connection.azure.ts
+++ accessible-places-backend/src/utils/connection.azure.ts
@@ -1,9 +1,13 @@
 import * as storage from '@azure/storage-blob';
+import { config } from 'dotenv';
 
+config()
 const accountname = process.env.AZURE_BLOB_ACCOUNT_NAME;
 const key = process.env.AZURE_BLOB_KEY;
 const containerName = process.env.AZURE_BLOB_CONTAINER;
 const linkTTL = process.env.AZURE_BLOB_SAS_TTL;
+
+console.log({accountname, key, containerName, linkTTL})
 
 const credentials = new storage.StorageSharedKeyCredential(accountname, key);
 const blobServiceClient = new storage.BlobServiceClient(
