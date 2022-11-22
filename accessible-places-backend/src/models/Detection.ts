e262434fbb9fde5b6a8f9106f2d50647cba2fac6
Andrei Remus Plesa <80645758+remusplesa@users.noreply.github.com>
2022-11-22 20:43:31 +0200

    fix: added x,y cordinates for bounding boxes edges

--- accessible-places-backend/src/models/Detection.ts
+++ accessible-places-backend/src/models/Detection.ts
@@ -1,24 +1,37 @@
 import { prop, getModelForClass } from '@typegoose/typegoose';
 import { Field, ObjectType, InputType } from 'type-graphql';
 
+
+@InputType('ImageCoordsInput')
+@ObjectType('ImageCoords')
+class ImageCoords {
+  @Field()
+  @prop({required: true})
+  x: number;
+
+  @Field()
+  @prop({required: true})
+  y: number;
+}
+
 @InputType('DetectionInput')
 @ObjectType('DetectionType')
 export class Detection {
   @Field()
   @prop({ required: true })
-  tl!: number;
+  tl!: ImageCoords;
 
   @Field()
   @prop({ required: true })
-  tr!: number;
+  tr!: ImageCoords;
 
   @Field()
   @prop({ required: true })
-  bl!: number;
+  bl!: ImageCoords;
 
   @Field()
   @prop({ required: true })
-  br!: number;
+  br!: ImageCoords;
 
   @Field()
   @prop({required: true})
