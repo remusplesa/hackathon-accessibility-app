80377c8418492d06a9aaffe7e1ed234df527d670
singularity-bit <maxdsl98@gmail.com>
2022-11-22 21:07:40 +0200

    refactor

--- accessible-places-ai/app/app.py
+++ accessible-places-ai/app/app.py
@@ -7,18 +7,25 @@ import argparse
 import io
 
 import torch
-from flask import Flask, request
+from flask import Flask, request,jsonify
 from PIL import Image
 
 app = Flask(__name__)
 print(torch.__version__)
 DETECTION_URL = "/v1/predict"
 
-model = torch.hub.load("ultralytics/yolov5", 'custom', path='./model/best.pt')
+model = torch.hub.load("ultralytics/yolov5", 'custom','./model/best.pt' )
 model.conf=0.5
 
+def batch_predict(image_bytes):
+    im = Image.open(io.BytesIO(image_bytes))
+    results = model(im, size=640) 
+    
+    return results.pandas().xyxy[0].to_json(orient="records")
+
+
 @app.route(DETECTION_URL, methods=["POST"])
-def predict():
+def stream_predict():
     if request.method != "POST":
         return
 
@@ -26,16 +33,12 @@ def predict():
 
         im_file = request.files["image"]
         im_bytes = im_file.read()
-        im = Image.open(io.BytesIO(im_bytes))
-
-        
-        results = model(im, size=640) 
-        return results.pandas().xyxy[0].to_json(orient="records")
-
+        output=batch_predict(im_bytes)
+        return output
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Flask API exposing YOLOv5 model")
-    parser.add_argument("--port", default=5001, type=int, help="port number")
+    parser.add_argument("--port", type=int, help="port number")
     opt = parser.parse_args()
 
 
