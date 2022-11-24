9bd0572920486dbb307843f497859aeea175a6e9
dinu.balasoiu <dinu.balasoiu@ibm.com>
2022-11-24 20:00:47 +0200

    added the rectangle and made it resizable

---
+++ accessible-places-frontend/src/components/RectangleShape/RectangleShape.tsx
@@ -0,0 +1,81 @@
+import React from "react";
+import { Rect, Transformer } from "react-konva";
+
+export const RectangleShape = ({
+  shapeProps,
+  isSelected,
+  onSelect,
+  onChange,
+}: Props) => {
+  const shapeRef: any = React.useRef();
+  const trRef: any = React.useRef();
+
+  React.useEffect(() => {
+    if (isSelected) {
+      // we need to attach transformer manually
+      trRef.current.nodes([shapeRef.current]);
+      trRef.current.getLayer().batchDraw();
+    }
+  }, [isSelected]);
+
+  return (
+    <React.Fragment>
+      <Rect
+        onClick={onSelect}
+        onTap={onSelect}
+        ref={shapeRef}
+        {...shapeProps}
+        draggable
+        onDragEnd={(e) => {
+          onChange({
+            ...shapeProps,
+            x: e.target.x(),
+            y: e.target.y(),
+          });
+        }}
+        onTransformEnd={(e) => {
+          // transformer is changing scale of the node
+          // and NOT its width or height
+          // but in the store we have only width and height
+          // to match the data better we will reset scale on transform end
+          const node: any = shapeRef.current;
+          if (node) {
+            const scaleX = node.scaleX();
+            const scaleY = node.scaleY();
+
+            // we will reset it back
+            node.scaleX(1);
+            node.scaleY(1);
+            onChange({
+              ...shapeProps,
+              x: node.x(),
+              y: node.y(),
+              // set minimal value
+              width: Math.max(5, node.width() * scaleX),
+              height: Math.max(node.height() * scaleY),
+            });
+          }
+        }}
+      />
+      {isSelected && (
+        <Transformer
+          ref={trRef}
+          boundBoxFunc={(oldBox, newBox) => {
+            // limit resize
+            if (newBox.width < 5 || newBox.height < 5) {
+              return oldBox;
+            }
+            return newBox;
+          }}
+        />
+      )}
+    </React.Fragment>
+  );
+};
+
+type Props = {
+  shapeProps: any;
+  isSelected: boolean;
+  onSelect: (data: any) => void;
+  onChange: (data: any) => void;
+};
--- accessible-places-frontend/src/components/ShapeEditor/ShapeEditor.tsx
+++ accessible-places-frontend/src/components/ShapeEditor/ShapeEditor.tsx
@@ -1,17 +1,58 @@
-import { useEffect, useRef, useState } from "react";
+import { useState } from "react";
 import { Stage, Layer, Rect, Image } from "react-konva";
-import { convertBase64, convertImageToCanvas } from "../../utils/utils";
+import { convertImageToCanvas } from "../../utils/utils";
+import { RectangleShape } from "../RectangleShape/RectangleShape";
 
 export function ShapeEditor({ selectedFile }: Props) {
   const image = convertImageToCanvas(selectedFile);
+  const rectProps = [
+    {
+      x: 10,
+      y: 10,
+      width: 100,
+      height: 100,
+      stroke: "red",
+      strokeWidth: 2,
+      id: "rect1",
+    },
+  ];
+  const [selectedId, selectShape] = useState<any>(null);
+  const [rectangles, setRectangles] = useState<any>(rectProps);
+
+  const checkDeselect = (e: any) => {
+    const clickedOnEmpty = e.target === e.target.getStage();
+    if (clickedOnEmpty) {
+      selectShape(null);
+    }
+  };
+
   return (
     <>
-      {selectedFile && (
+      {image && (
         <>
-          <Stage width={window.innerWidth} height={window.innerHeight}>
+          <Stage
+            width={Number(image.width)}
+            height={Number(image.height)}
+            onMouseDown={checkDeselect}
+            onTouchStart={checkDeselect}
+          >
             <Layer>
               <Image image={image} />
-              <Rect width={50} height={50} fill="red" />
+              {rectangles.map((rect: any, i: number) => (
+                <RectangleShape
+                  key="positionRect"
+                  shapeProps={rect}
+                  isSelected={rect.id === selectedId}
+                  onSelect={() => {
+                    selectShape(rect.id);
+                  }}
+                  onChange={(newAttrs) => {
+                    const rects = rectangles.slice();
+                    rects[i] = newAttrs;
+                    setRectangles(rects);
+                  }}
+                />
+              ))}
             </Layer>
           </Stage>
         </>
