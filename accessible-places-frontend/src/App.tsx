f5ed1ca51cf24a69e266158cb5e149c5efb96b7c
Cristian-Alin Puican <puican.ca@gmail.com>
2022-11-25 01:43:43 +0200

    Added side drawer

--- accessible-places-frontend/src/App.tsx
+++ accessible-places-frontend/src/App.tsx
@@ -4,19 +4,22 @@ import { MapPage } from "./pages/MapPage/MapPage";
 import { LoginPage } from "./pages/LoginPage/LoginPage";
 
 import PrivateRoute from "./components/PrivateRoute/PrivateRoute";
+import { SideDrawerContextProvider } from "./Context/SideDrawerContext/SideDrawerContext";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/login" element={<LoginPage />} />
-      <Route path="/" element={<div>Accessible Places - Frontend</div>} />
-      <Route path="/upload" element={<PrivateRoute />}>
-        <Route path="/upload" element={<ImageUpload />} />
-      </Route>
-      <Route path="/map" element={<PrivateRoute />}>
-        <Route path="/map" element={<MapPage />} />
-      </Route>
-    </Routes>
+    <SideDrawerContextProvider>
+      <Routes>
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/" element={<MapPage />} />
+        <Route path="/upload" element={<PrivateRoute />}>
+          <Route path="/upload" element={<ImageUpload />} />
+        </Route>
+        <Route path="/map" element={<PrivateRoute />}>
+          <Route path="/map" element={<MapPage />} />
+        </Route>
+      </Routes>
+    </SideDrawerContextProvider>
   );
 }
 
