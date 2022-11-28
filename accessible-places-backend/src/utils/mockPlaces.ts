09a1235199f8f75dfaae085971d295c6c6f34a42
Andrei Remus Plesa <80645758+remusplesa@users.noreply.github.com>
2022-11-28 23:21:53 +0200

    fix: moved Places mock function

---
+++ accessible-places-backend/src/utils/mockPlaces.ts
@@ -0,0 +1,48 @@
+import { faker } from '@faker-js/faker';
+import { PlaceResult } from '../models';
+
+export const mockPlaces = (
+  latMax: number,
+  latMin: number,
+  lngMax: number,
+  lngMin: number,
+  itemsNumber: number = 20
+): PlaceResult[] => {
+  const places = [];
+  for (let i = 0; i < itemsNumber; i++) {
+    places.push({
+      _id: faker.datatype.uuid(),
+      poiName: faker.company.name(),
+      isAccessible: faker.datatype.boolean(),
+      photos: (() => {
+        let photos = [];
+        for (let i = 0; i < faker.datatype.number({ min: 1, max: 6 }); i++) {
+          photos.push({
+            id: faker.datatype.uuid(),
+            url: faker.image.abstract(),
+            detections: [
+              {
+                id: faker.datatype.uuid(),
+                xMax: faker.datatype.number({ min: 10, max: 600 }),
+                xMin: faker.datatype.number({ min: 10, max: 600 }),
+                yMax: faker.datatype.number({ min: 10, max: 600 }),
+                yMin: faker.datatype.number({ min: 10, max: 600 }),
+                label: faker.helpers.arrayElement(['stairs', 'ramp']),
+              },
+            ],
+          });
+        }
+        return photos;
+      })(),
+      accesibilityDetails: {
+        parking: faker.datatype.boolean(),
+        elevator: faker.datatype.boolean(),
+      },
+      coordinates: {
+        lat: Number(faker.address.latitude(latMax, latMin, 6)),
+        lng: Number(faker.address.longitude(lngMax, lngMin, 6)),
+      },
+    });
+  }
+  return places;
+};
