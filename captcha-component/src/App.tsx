9e522b905054dd817555bcb75b86b7d12c34f868
Andrei Plesa <Andrei.Plesa@ibm.com>
2022-12-05 11:55:38 +0200

    feat: fetch challange for captcha

--- captcha-component/src/App.tsx
+++ captcha-component/src/App.tsx
@@ -1,7 +1,13 @@
+import React from "react";
 import "./App.css";
+import { CaptchaComponent } from './components/CaptchaComponent';
 
 function App() {
-  return <div className="App">App</div>;
+  return <div className="App">
+    <form>
+      <CaptchaComponent />
+    </form>
+  </div>;
 }
 
 export default App;
--- captcha-component/src/components/CaptchaComponent.tsx
+++ captcha-component/src/components/CaptchaComponent.tsx
@@ -1,9 +1,77 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 interface Props {
-  labelText: string;
+  // labelText: string;
 }
 
-export const CaptchaComponent: React.FC<Props> = ({
-  labelText,
-}: Props): JSX.Element => <div>{labelText}</div>;
+const options = {
+  method: "POST",
+  url: "http://localhost:4001/graphql",
+  headers: { "Content-Type": "application/json" },
+  data: {
+    query: `query GetChallenge($mocked: Boolean) {
+               getChallenge(mocked: $mocked){
+                 quizzId
+                 photoUrl
+                 challenge {
+                  id
+                  name
+                  xmin
+                  xmax
+                  ymin
+                  ymax
+                }
+              }
+            }
+            `,
+    variables: { mocked: true },
+    operationName: "GetChallenge",
+  },
+};
+
+export const CaptchaComponent: React.FC<Props> = (): JSX.Element => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [isValid, setIsValid] = useState(false);
+  const [challenge, setChallenge] = useState<ChallengeResponse>();
+
+  useEffect(() => {
+    setIsLoading(true);
+    (async () => {
+      const res = await axios(options);
+      if (res.data) {
+        setChallenge(res.data);
+      }
+    })();
+    setIsLoading(false);
+  }, []);
+
+  return (
+    <>
+      {isLoading ? (
+        "Loading..."
+      ) : (
+        <input required>
+          <pre>{JSON.stringify(challenge, null, 2)}</pre>
+        </input>
+      )}
+    </>
+  );
+};
+
+export interface IPrediction {
+  id: string;
+  name: string;
+  xmin: number;
+  xmax: number;
+  ymin: number;
+  ymax: number;
+}
+
+type ChallengeResponse = {
+  getChallenge: {
+    quizzId: string;
+    photoUrl: string;
+    challenge: IPrediction;
+  };
+};
