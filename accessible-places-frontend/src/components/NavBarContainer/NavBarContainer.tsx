be2300f04004871846bced32cf25f6bd307a8941
dinu.balasoiu <dinu.balasoiu@ibm.com>
2022-12-06 20:36:11 +0200

    max width to navbar

--- accessible-places-frontend/src/components/NavBarContainer/NavBarContainer.tsx
+++ accessible-places-frontend/src/components/NavBarContainer/NavBarContainer.tsx
@@ -8,6 +8,7 @@ export const NavBarContainer = ({ children, ...props }: any) => {
       justify="space-between"
       wrap="wrap"
       w="100%"
+      maxW="1920px"
       p={8}
       bg={["#4a9f66", "#4a9f66", "transparent", "transparent"]}
       color={["white", "white", "#83988b", "#83988b"]}
